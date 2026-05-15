dab833eceaef0e0e7ee7ce425a37b518ed181be4
Manav Mahendrakumar Varia <164327048+ManavVaria@users.noreply.github.com>
2026-05-15 08:23:16 +0530

    First project in this course, its just combining two inputs from the user

---
+++ Project_1(Band name generator).py
@@ -0,0 +1,8 @@
+# Project 1 - Band name generator
+
+print("Welcome to the band name generator")
+
+city = input("What is the name of the city you grew up in ?\n")
+favourite_name = input("What is the name of your friends group ?\n")
+
+print("Your band name could be ", city + " " + favourite_name)
